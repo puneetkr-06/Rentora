19331031f4f75a74e83fe7127b60cd5a4df22d2a
Puneet Kumar <puneetkr.06@gmail.com>
2026-06-14 10:32:12 +0530

    Added Frontend URL with Cors in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://rentora-gamma.vercel.app/'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Route Middleware
